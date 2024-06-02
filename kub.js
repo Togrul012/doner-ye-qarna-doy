6744acabb40b95ab03c770cdb966f7dfad48c03c
togrull2000 <togrull2000@gmail.com>
2024-06-02 16:20:41 +0400

    yem

---
+++ kub.js
@@ -0,0 +1,18 @@
+let ad = "Togrul" ;
+let yemek = "Qutab" ;
+let email = "bbs62551661@gmail.com" ;
+
+let finish = true;
+let online = false;
+let sale = true;
+
+let yas = 21;
+let user = 2;
+let say = 3;
+let qiymet = 1.00;
+
+let total = null;
+
+console.log("Siz " + say + " eded " + yemek + " sifaris etmisiniz! ");
+total = say * qiymet;
+console.log("Siz de hesab " + total + " AZN edecek!");
